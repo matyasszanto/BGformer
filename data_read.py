fb1e525ca17d595b94507abcb47ba11cb5cf7410
maty@remote_MBA <matyasszanto@gmail.com>
2024-03-05 17:09:31 +1300

    MOD: added data cleaning - short seqs and zeros removed.

--- data_read.py
+++ data_read.py
@@ -22,7 +22,38 @@ for i, patient in enumerate(pd.unique(df["idx"])):
     time_series_s.at[i, 'id'] = patient
     time_series_s.at[i, 'values'] = current_SI_vals
 
-print(time_series_s.tail())
+# print(time_series_s.tail())
 
-plt.plot(time_series_s.at[5, 'values'])
-plt.show()
+# plt.plot(time_series_s.at[5, 'values'])
+# plt.show()
+
+print(f'number of series before reduction: {len(time_series_s)}')
+
+lens = []
+max_len_index = 0
+for i, time_series in enumerate(time_series_s.iterrows()):
+    curr_len = len(time_series[1][1])
+    if len(lens) > 0 and curr_len > max(lens):
+        max_len_index = i
+        longest = time_series
+    if curr_len < 38:
+        time_series_s.drop(index=i, inplace=True)
+    else:
+        lens.append(curr_len)
+        numzeros = 0
+        for el in time_series[1][1]:
+            numzeros += 1 if el == 0 else 0
+        if numzeros > curr_len / 15:
+            time_series_s.drop(index=i, inplace=True)
+
+longest_df = pd.DataFrame(longest)
+print(longest_df.head())           
+
+
+print(f'number of series after reduction: {len(time_series_s)}')
+print(f'max = {max(lens)}')
+print(f'min = {min(lens)}')
+
+print(f'max_len_index: {max_len_index}')
+
+print(time_series_s.head())
