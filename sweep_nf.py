9f5be2fb689d274740233abf06af7eeb1cf9791d
Maty@Hulk <matyasszanto@gmail.com>
2024-05-08 11:18:13 +1200

    ADD: wandb run created for logging results

---
+++ sweep_nf.py
@@ -0,0 +1,29 @@
+import wandb
+import numpy as np
+import train, test
+from neuralforecast.models import NHITS, TimesNet, FEDformer, Informer, Autoformer
+
+config = {
+        'models': [TimesNet, FEDformer, Informer, Autoformer]
+    }
+wandb.init(
+    entity='szanto-matyas',
+    project='BGformer',
+    config=config,
+)
+
+nf, plot_train = train.run(config=config)
+plot_test, metrics = test.run(models = nf)
+
+image_train = wandb.Image(plot_train, caption=f'train plots')
+image_test = wandb.Image(plot_test, caption=f'test plots')
+
+wandb.log(
+    {
+     'training plots': image_train,
+     'test plots': image_test,
+     'metrics': metrics,
+    }
+)
+
+wandb.finish()
