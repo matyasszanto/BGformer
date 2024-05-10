218db6421011962c5ebdded7b0146200bacd70a1
Maty@Hulk <matyasszanto@gmail.com>
2024-05-10 18:38:44 +1200

    ADD: sweep set up for BGformer OHIO train + ICU test

---
+++ BGformer_sweep_1.py
@@ -0,0 +1,57 @@
+import wandb
+import numpy as np
+import train, test
+
+
+# init weights and biases
+wandb.login()
+
+
+# experiment
+def experiment():
+    
+    wandb.init()
+    
+    nf, plot_train = train.run(config=wandb.config)
+    plot_test, metrics = test.run(models=nf)
+
+    image_train = wandb.Image(plot_train, caption=f'train plots')
+    image_test = wandb.Image(plot_test, caption=f'test plots')
+
+    wandb.log(
+        {
+        'training plots': image_train,
+        'test plots': image_test,
+        'metrics': metrics,
+        'best RMSE': min(metrics['RMSE'])
+        }
+    )
+
+
+# set up sweep config
+config = {
+    'method': 'grid',
+    # 'run_cap': 5,
+    'metric': {
+        'goal': 'minimize',
+        'name': 'best RMSE'
+    },
+    'parameters': {
+        # model types: NHITS, TimesNet, FEDformer, Informer, Autoformer, iTransformer
+        'models': {
+            'values': ['TimesNet', 'FEDformer', 'Autoformer'],
+        },
+        # 'TN_topk': {
+        #     'values': [2, 3, 4, 5, 6]
+        # }
+    }
+}
+
+
+# set up sweep and run it
+sweep_id = wandb.sweep(sweep=config, project='BGformer')
+wandb.agent(sweep_id=sweep_id, function=experiment)
+
+
+# clean up
+wandb.finish()
