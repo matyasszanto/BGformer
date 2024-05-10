c1b16fff8c4a7983f1f041900a7e295e581f50a0
Maty@Hulk <matyasszanto@gmail.com>
2024-05-10 18:41:27 +1200

    MOD: fix for wandb sweep agent (no objects passable in config), debug mode added

--- train.py
+++ train.py
@@ -4,11 +4,19 @@ from utils import to_datetime, connect_gt_and_pred
 
 from statsforecast import StatsForecast
 from neuralforecast import NeuralForecast
+from neuralforecast import core
 
-def run(config = None):
-    if config['models'] == []:
+def run(config=None, debug=False):
+
+    if debug:
+        config = {"models": "NHITS"}
+
+    elif config['models'] == []:
         print("Error: no model configured!")
         return None, None
+
+    elif 'TN_topk' not in config.keys():
+        config['TN_topk'] = 3
     
     Y_df = pd.read_csv("preliminary/full_dataset_hourly.csv")
     timestamps = Y_df['ds']
@@ -20,8 +28,19 @@ def run(config = None):
     
     freq = 'h' # h or min
     horizon = 3 if freq=='h' else 90
+    model_strings_array = [config['models']] if isinstance(config['models'], str) else config['models']
+    models_array = []
+    for model in model_strings_array:
+        model_object = getattr(core, model)
+        if model == "iTransformer":
+            models_array.append(model_object(h=horizon, input_size=20, max_steps=1, n_series=1))
+        elif model == "TimesNet":
+            models_array.append(model_object(h=horizon, input_size=20, max_steps=1, top_k=config['TN_topk']))
+        else:
+            models_array.append(model_object(h=horizon, input_size=20, max_steps=1))
+    
     nf = NeuralForecast(
-        models=[model(h=horizon, input_size=20, max_steps=1) for model in config['models']],
+        models=models_array,
         freq=freq
     )
 
@@ -32,13 +51,13 @@ def run(config = None):
     plot = StatsForecast.plot(Y_df, df_to_plot.drop(columns=['y', 'cutoff']), max_insample_length=1260)
 
     print('\n\n\n')
-    print("----------------------------")
-    print("--------Training done-------")
-    print("----------------------------")
+    print("-----------------------------")
+    print("--------Training done--------")
+    print("-----------------------------")
     print('\n\n\n')
 
     return nf, plot
 
 if __name__ == '__main__':
-    run()
+    run(debug=True)
     
