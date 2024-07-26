34bcf68a76efff2fec2479d6672a01ee26343845
Maty@Hulk <matyasszanto@gmail.com>
2024-07-27 01:48:44 +1200

    MOD: added random snippet generation. Refactored gt and pred connection for more efficient execution.

--- utils.py
+++ utils.py
@@ -1,49 +1,50 @@
 import datetime as dt
 import pandas as pd
+import numpy as np
+from tqdm import tqdm
 
 
 def to_datetime(input_string):
     return dt.datetime.strptime(input_string, "%Y-%m-%d %H:%M:%S").replace(minute=0, second=0)
 
 
-def connect_gt_and_pred(df_gt, df_pred):
-    Y_df_2 = df_gt.set_index('ds')
+def connect_gt_and_pred(df_gt, df_pred, horizon=3):
 
-    cv_df_2 = df_pred.reset_index()
+    # debug
+    print('Started merging function')
+    
+    # local copies
+    Y_df_2 = df_gt.reset_index()
+    pred_df_2 = df_pred.reset_index()
+
+    # create list of cutoff timestamps
     cutoff_stamps = []
-    if 'cutoff' not in cv_df_2.columns:
-        for i in range(len(cv_df_2)):
-            hours_to_subtract = i%3 + 1
-            cutoff_stamp = cv_df_2.at[i, 'ds'] - dt.timedelta(hours=hours_to_subtract, minutes=0)
+    if 'cutoff' not in pred_df_2.columns:
+        for i in range(len(pred_df_2)):
+            hours_to_subtract = i%horizon + 1
+            cutoff_stamp = pred_df_2.at[i, 'ds'] - dt.timedelta(hours=hours_to_subtract, minutes=0)
             cutoff_stamps.append(cutoff_stamp)
         
-        cv_df_2['cutoff'] = cutoff_stamps
+        pred_df_2['cutoff'] = cutoff_stamps
+
+    print('Cutoff timestamps appended')
 
     cv_df_output = pd.DataFrame()
     added_rows = 0
-    for i in range(len(cv_df_2)):
-        if i % 3 == 0:
-            cv_df_output = pd.concat([cv_df_output, cv_df_2.iloc[i].to_frame().transpose(), cv_df_2.iloc[i:i + 3]], axis=0)
+    gt_uids = Y_df_2['unique_id'][::24].to_numpy()
+    for i in tqdm(range(len(pred_df_2))):
+        if i % horizon == 0:
+            
+            # fill cv_df_output with predictions and an extra row for t=0
+            cv_df_output = pd.concat([cv_df_output, pred_df_2.iloc[i].to_frame().transpose(), pred_df_2.iloc[i:i + horizon]], axis=0)
             cv_df_output.reset_index(drop=True, inplace=True)
             cv_df_output.at[i+added_rows, 'ds'] = cv_df_output.at[i+added_rows, 'cutoff']
-            cutoff_timestamp = cv_df_2.iloc[i]['cutoff']
-            unique_id = cv_df_2.iloc[i]['unique_id']
             
-            while True:
-                try:
-                    gt_row = Y_df_2.loc[cutoff_timestamp]
-                    break
-                except:
-                    cutoff_timestamp = cutoff_timestamp - dt.timedelta(hours=1, minutes=0)
-                    
-            if type(gt_row) == type(pd.DataFrame()):
-                for j, row in enumerate(Y_df_2.loc[cutoff_timestamp].iterrows()):
-                    if row[1]['unique_id'] == unique_id:
-                        bg_gt = row[1]['y']
-                        break
-        
-            else:
-                bg_gt = Y_df_2.loc[cutoff_timestamp]['y']
+            # find current ground truth slice to merge
+            pred_uid = pred_df_2.at[i, 'unique_id']
+            current_gt_slice_index = np.where(gt_uids==pred_uid)[0].item()
+            
+            bg_gt = Y_df_2.loc[(current_gt_slice_index+1)*24-horizon-1]['y']
 
             for col in cv_df_output.columns:
                 if col not in ['unique_id', 'ds', 'cutoff']:
@@ -53,10 +54,27 @@ def connect_gt_and_pred(df_gt, df_pred):
 
     cv_df_output.set_index('ds')
 
-    if 'y' not in cv_df_2.columns:
+    if 'y' not in pred_df_2.columns:
         cv_df_output['y'] = cv_df_output['cutoff']
 
     if 'index' in cv_df_output.columns:
         cv_df_output.drop('index', axis=1, inplace=True)
 
     return cv_df_output
+
+
+# Function to find all continuous snippets
+def find_all_continuous_snippets(df, hours=24):
+    snippets = []
+    total_points = len(df)
+    snippet_counter = 0
+    
+    for start_idx in range(total_points - hours + 1):
+        snippet = df.iloc[start_idx:start_idx + hours].copy()
+        time_diffs = snippet['ds'].diff().iloc[1:]  # Skip the first NaT value
+        if all(time_diffs == pd.Timedelta(hours=1)):
+            snippet.loc[:, 'unique_id'] = f"{snippet['unique_id'].iloc[0]}_{snippet_counter}"
+            snippets.append(snippet)
+            snippet_counter += 1
+    
+    return pd.concat(snippets).reset_index(drop=True) if snippets else pd.DataFrame()
