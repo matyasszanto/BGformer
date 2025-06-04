e9609c13a78ab46843048e7e6503363a3e343526
Maty@Hulk <matyasszanto@gmail.com>
2025-06-05 02:49:08 +1200

    MOD: SIformer training testrun

--- testrun.py
+++ testrun.py
@@ -8,24 +8,24 @@ config = {
         'models': ["TimesNet"],
         # datasets: ICU, Ohio
         'train_dataset': 'ICU_train',
-        'test_dataset': 'ICU_test',
+        'test_dataset': 'ICU_test_10',
         'normalize': False,
         'TN_topk': 4,
         'max_steps': 1000,
         'val_check_steps': 500,
-        'horizon': 6,
-        'snippet_length': 27,
+        'horizon': 3,
+        'snippet_length': 24,
         'enable_checkpointing': True,
     }
 if not debug:
     wandb.init(
         entity='szanto-matyas',
-        project='BGformer_normalized',
+        project='SIformer_models_train',
         config=config,
     )
 
 nf = train.run(config=config)
-prediction_plots, metrics, prediction_hists, means_stds_df = test.run(config=config, models = nf)
+prediction_plots, metrics, prediction_hists, means_stds_df = test.run(config=config, models=nf)
 
 if not debug:
     image_test = wandb.Image(prediction_plots, caption='Test plots')
